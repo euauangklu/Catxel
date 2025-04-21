8aec080a79f9fa0600d3b0e1fd58ae68d6d5d118
Raiden <euauangklu.2234@gmail.com>
2025-04-21 15:51:33 +0700

    CreateProject

---
+++ Assets/BehaviourScript/AreaWalkAction.cs
@@ -0,0 +1,100 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+using Random = UnityEngine.Random;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "AreaWalk", story: "[Agent] walk in area", category: "Action", id: "d01e312d663ee9eb535d81240cea06bc")]
+public partial class AreaWalkAction : Action
+{
+    [SerializeReference] public BlackboardVariable<GameObject> Agent;
+    
+    [SerializeReference] public BlackboardVariable<Transform> AreaTopLeft;
+
+    [SerializeReference] public BlackboardVariable<Transform> AreaBottomRight;
+
+    [SerializeReference] public BlackboardVariable<float> WalkSpeed;
+    
+    [SerializeReference] public BlackboardVariable<float> WaitTime = new BlackboardVariable<float>(1.0f);
+    
+    [SerializeReference] public BlackboardVariable<string> AnimatorSpeedParam = new BlackboardVariable<string>("SpeedMagnitude");
+    
+    private float WaitTimer;
+    
+    private bool Waiting;
+
+    private Vector2 targetPosition;
+    
+    private Vector2 RecentPosition;
+
+    private bool FacingRight;
+    
+    private Animator Animator;
+
+    protected override Status OnStart()
+    {
+        PickNewTarget();
+        RecentPosition = Agent.Value.transform.position;
+        WaitTimer = WaitTime.Value;
+        Animator = Agent.Value.GetComponentInChildren<Animator>();
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        Agent.Value.transform.position = Vector2.MoveTowards(Agent.Value.transform.position, targetPosition, WalkSpeed * 100f * Time.deltaTime);
+        if (Vector2.Distance(Agent.Value.transform.position, targetPosition) < 0.1f)
+        {
+            Waiting = true;
+        }
+
+        if (Waiting)
+        {
+            Animator.SetFloat(AnimatorSpeedParam,0);
+            if (WaitTimer > 0f)
+            {
+                WaitTimer -= Time.deltaTime;
+            }
+            else
+            {
+                Waiting = false;
+                return Status.Success;
+            }
+        }
+        else if (!Waiting)
+        {
+            Animator.SetFloat(AnimatorSpeedParam,1);
+            if (RecentPosition.x > targetPosition.x && !FacingRight)
+            {
+                Flip();
+            }
+            if (RecentPosition.x < targetPosition.x && FacingRight)
+            {
+                Flip();
+            }
+        }
+        return Status.Running;
+    }
+
+    protected override void OnEnd()
+    {
+    }
+    
+    private void PickNewTarget()
+    {
+        float x = Random.Range(AreaTopLeft.Value.position.x, AreaBottomRight.Value.position.x);
+        float y = Random.Range(AreaBottomRight.Value.position.y, AreaTopLeft.Value.position.y);
+        targetPosition = new Vector2(x, y);
+    }
+
+    private void Flip()
+    {
+        Vector2 CurrentScale = Agent.Value.transform.localScale;
+        CurrentScale.x *= -1;
+        Agent.Value.transform.localScale = CurrentScale;
+        FacingRight = !FacingRight;
+    }
+}
+
---
+++ Assets/BehaviourScript/SleepAction.cs
@@ -0,0 +1,50 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "Sleep", story: "[Agent] Sleep", category: "Action", id: "8cb1e684f2ad5fb9f1a2b3fc929bff6c")]
+public partial class SleepAction : Action
+{
+    [SerializeReference] public BlackboardVariable<GameObject> Agent;
+    
+    [SerializeReference] public BlackboardVariable<float> WalkSpeed;
+    
+    [SerializeReference] public BlackboardVariable<float> SleepTime = new BlackboardVariable<float>(1.0f);
+    
+    [SerializeReference] public BlackboardVariable<string> AnimatorSleepParam = new BlackboardVariable<string>("Sleep");
+
+    private Animator Animator;
+    
+    private float SleepTimer;
+    
+    protected override Status OnStart()
+    {
+        Animator = Agent.Value.GetComponentInChildren<Animator>();
+        SleepTimer = SleepTime.Value;
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        if (SleepTimer > 0)
+        {
+            SleepTimer -= Time.deltaTime;
+            Animator.SetBool(AnimatorSleepParam,true);
+        }
+        if (SleepTimer <= 0f)
+        {
+            Animator.SetBool(AnimatorSleepParam,false);
+            return Status.Success;
+        }
+        return Status.Running;
+    }
+
+    protected override void OnEnd()
+    {
+        Animator.SetBool(AnimatorSleepParam,false);
+    }
+}
+
